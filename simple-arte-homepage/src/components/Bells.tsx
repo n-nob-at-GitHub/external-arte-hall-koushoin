46f58b87d628a5848e9825bb00c845e17a0bf803
n-nob-at-GitHub <sub.nakamura.xyz@gmail.com>
2025-05-12 07:08:31 +0900

    簡易ホームページ追加対応（御鈴レイアウト修正）

--- simple-arte-homepage/src/components/Bells.tsx
+++ simple-arte-homepage/src/components/Bells.tsx
@@ -1,30 +1,70 @@
 'use client'
-import Image from 'next/image'
 import {
-  Grid,
-  Paper,
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
+  Typography
 } from '@mui/material'
-import image144 from '@/images/144.png'
+import image144_1 from '@/images/144-1.png'
+import image144_2 from '@/images/144-2.png'
+import image144_3 from '@/images/144-3.png'
+
+const srcset = (image: string, size: number, rows = 1, cols = 1) => {
+  return {
+    src: `${ image }?w=${ size * cols }&h=${ size * rows }&fit=crop&auto=format`,
+    srcSet: `${ image }?w=${ size * cols }&h=${
+      size * rows
+    }&fit=crop&auto=format&dpr=2 2x`,
+  };
+}
 
 const Bells = () => {
-  const images = [ image144 ];
-  const imagesContainer = images.map((image, index) =>
-    <Grid size={ 4 } key={ index }>
-      <Image
-        src={ image.src }
-        width={ 830 }
-        height={ 407 }
-        alt='Bell Image'
-        style={{ objectFit: 'contain' }}
-      />
-    </Grid>
-  )
+  const itemData = [
+    {
+      img: image144_1,
+      title: 'ピンク',
+      detail: '24,200円（税込） Φ53×H53',
+      rows: 1,
+      cols: 1,
+    },
+    {
+      img: image144_2,
+      title: 'ミント',
+      detail: '24,200円（税込） Φ53×H53',
+      rows: 1,
+      cols: 1,
+    },
+    {
+      img: image144_3,
+      title: 'スノー',
+      detail: '24,200円（税込） Φ53×H53',
+      rows: 1,
+      cols: 1,
+    }
+  ];
   return (
-    <Paper>
-      <Grid container spacing={ 2 }>
-        { imagesContainer }
-      </Grid>
-    </Paper>
+    <>
+      <Typography variant='h5'>たまゆら パステル</Typography>
+      <ImageList sx={{ width: 1000, height: 800 }}
+        cols={ 4 }
+        rowHeight={ 250 }
+        >
+        { itemData.map((item, index) => (
+          <ImageListItem key={ index } cols={ item.cols || 1 } rows={ item.rows || 1 }>
+            <img
+              { ...srcset(item.img.src, 250, item.rows, item.cols) }
+              alt={ item.title }
+              loading='lazy'
+            />
+            <ImageListItemBar
+              title={ item.title }
+              subtitle={ item.detail }
+              position='below'
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </>
   )
 }
 
