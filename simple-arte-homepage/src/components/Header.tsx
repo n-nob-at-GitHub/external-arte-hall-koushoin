6b57c73df0e2da41c001b7720878e31825e37f48
AT2308-01\AT-USER <>
2025-05-13 14:31:27 +0900

    簡易ホームページ追加対応（ヘッダーレイアウト修正）

--- simple-arte-homepage/src/components/Header.tsx
+++ simple-arte-homepage/src/components/Header.tsx
@@ -9,8 +9,8 @@ import logo from '@/images/logo.svg'
 
 const Header = () => {
   return (
-    <Grid container spacing={ 2 }>
-      <Grid size={ 3 }>
+    <Grid container direction='row' wrap='nowrap' spacing={ 2 }>
+      <Grid size={{ xs: 6, sm: 4, md: 3 }}>
         <Image
           src={ logo.src }
           width={ 220 }
@@ -19,21 +19,27 @@ const Header = () => {
           style={{ objectFit: 'contain' }}
         />
       </Grid>
-      <Grid size={ 6 }></Grid>
-      <Grid size={ 3 }>
+      <Grid size={{ xs: false, sm: 1, md: 1 }}></Grid>
+      <Grid size={{ xs: 6, sm: 7, md: 8 }}>
         <Typography noWrap sx={{ 
           fontFamily: '游明朝, Yu Mincho, Noto Serif JP', 
           color: '#7d5a1e', 
-          fontSize: 'calc(0.4em + 1.0vw)',
-          textAlign: 'right'
+          fontSize: { xs: '1.2rem', sm: 'calc(0.4em + 1.0vw)', md: '1rem' },
+          textAlign: 'right',
+          whiteSpace: 'nowrap',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
         }}>
           365日 24時間いつでも受付
         </Typography>
         <Typography noWrap sx={{ 
           fontFamily: '游明朝, Yu Mincho, Noto Serif JP', 
           color: '#357a3b', 
-          fontSize: 'calc(0.5em + 1.6vw)',
-          textAlign: 'right'
+          fontSize: { xs: '1.4rem', sm: 'calc(0.5em + 1.6vw)', md: '1.2rem' },
+          textAlign: 'right',
+          whiteSpace: 'nowrap',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
         }}>
           TEL: 0265-81-1239
         </Typography>
