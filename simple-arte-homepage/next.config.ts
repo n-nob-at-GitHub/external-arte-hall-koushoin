dac18d1c182998f0c9a5c3609c7942add840f256
n-nob-at-GitHub <112742786+n-nob-at-GitHub@users.noreply.github.com>
2025-05-13 09:29:25 +0900

    Update next.config.ts

--- simple-arte-homepage/next.config.ts
+++ simple-arte-homepage/next.config.ts
@@ -10,9 +10,9 @@ const nextConfig: NextConfig = {
   // Use Static Export when running next build.
   output: 'export',
   // https://nextjs.org/docs/app/api-reference/config/next-config-js/basePath
-  basePath: isProd ? '/external-arte-hall-koushoin' : '',
+  // basePath: isProd ? '/external-arte-hall-koushoin' : '',
   // https://nextjs.org/docs/app/api-reference/config/next-config-js/assetPrefix
-  assetPrefix: isProd ? '/external-arte-hall-koushoin/' : '',
+  // assetPrefix: isProd ? '/external-arte-hall-koushoin/' : '',
   images: {
     unoptimized: true,
   },
