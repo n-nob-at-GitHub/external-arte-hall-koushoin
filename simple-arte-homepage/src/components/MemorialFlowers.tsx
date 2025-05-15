db436d497e9743bc06ec269c648c31297c902875
n-nob-at-GitHub <sub.nakamura.xyz@gmail.com>
2025-05-15 18:20:13 +0900

    簡易ホームページ追加対応（メモリアルフラワーレイアウト修正）

--- simple-arte-homepage/src/components/MemorialFlowers.tsx
+++ simple-arte-homepage/src/components/MemorialFlowers.tsx
@@ -1,31 +1,91 @@
 'use client'
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 import {
-  Grid,
-  Paper,
+  ImageList,
+  ImageListItem,
 } from '@mui/material'
-import image156 from '@/images/156.png'
-import image157 from '@/images/157.png'
+import image156_1 from '@/images/156-1.webp'
+import image156_2 from '@/images/156-2.webp'
+import image157_1 from '@/images/157-1.webp'
+import image157_2 from '@/images/157-2.webp'
+import image157_3 from '@/images/157-3.webp'
+
+type ImageItem = {
+  img: StaticImageData
+  title: string
+  cols?: number
+  rows?: number
+}
+
+const makeImageListContent = (data: ImageItem[], maxCols = 1) => {
+  return <ImageList 
+    cols={ maxCols }
+    sx={{
+      '& .MuiImageListItem-root': {
+        position: 'relative',
+      },
+    }}>
+    { data.map((item, index) => (
+      <ImageListItem 
+        key={ index }
+        cols={ item.cols } 
+        rows={ item.rows } 
+        sx={{
+          aspectRatio: `${ item.cols } / ${ item.rows }`,
+          border: 1, 
+          borderColor: 'grey.200', 
+          p: 2,
+          position: 'relative',
+        }}>
+        <Image
+          src={ item.img.src }
+          alt={ item.title }
+          loading='lazy'
+          fill
+          style={{ objectFit: 'cover' }}
+        />
+      </ImageListItem>
+    ))}
+  </ImageList>;
+}
 
 const MemorialFlowers = () => {
-  const images = [ image156, image157 ];
-  const imagesContainer = images.map((image, index) =>
-    <Grid size={ 4 } key={ index }>
-      <Image
-        src={ image.src }
-        width={ 613 }
-        height={ 820 }
-        alt='Memorial-Flower Image'
-        style={{ objectFit: 'contain' }}
-      />
-    </Grid>
-  )
+  const itemData = [
+    {
+      img: image156_1,
+      title: '',
+      rows: 2,
+      cols: 3,
+    },
+    {
+      img: image156_2,
+      title: '',
+      rows: 2,
+      cols: 3,
+    },
+    {
+      img: image157_1,
+      title: '',
+      rows: 1,
+      cols: 3,
+    },
+    {
+      img: image157_2,
+      title: '',
+      rows: 1,
+      cols: 3,
+    },
+    {
+      img: image157_3,
+      title: '',
+      rows: 1,
+      cols: 3,
+    }
+  ];
   return (
-    <Paper>
-      <Grid container spacing={ 70 }>
-        { imagesContainer }
-      </Grid>
-    </Paper>
+    <>
+      { makeImageListContent(itemData, 3) }
+    </>
   )
 }
 
