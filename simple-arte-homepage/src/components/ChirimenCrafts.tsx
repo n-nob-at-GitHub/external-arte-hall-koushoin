d68551fa9dc422f55062e03e092904a5cbdbfdff
n-nob-at-GitHub <sub.nakamura.xyz@gmail.com>
2025-05-15 19:21:47 +0900

    簡易ホームページ追加対応（ちりめん細工レイアウト修正）

--- simple-arte-homepage/src/components/ChirimenCrafts.tsx
+++ simple-arte-homepage/src/components/ChirimenCrafts.tsx
@@ -1,40 +1,62 @@
 'use client'
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 import {
-  Grid,
-  Paper,
-  Typography,
+  ImageList,
+  ImageListItem,
 } from '@mui/material'
-import image154 from '@/images/154.png'
+import image154 from '@/images/154.webp'
+
+type ImageItem = {
+  img: StaticImageData
+  title: string
+  cols?: number
+  rows?: number
+}
+
+const makeImageListContent = (data: ImageItem[], maxCols = 1) => {
+  return <ImageList 
+    cols={ maxCols }
+    sx={{
+      '& .MuiImageListItem-root': {
+        position: 'relative',
+      },
+    }}>
+    { data.map((item, index) => (
+      <ImageListItem 
+        key={ index }
+        cols={ item.cols } 
+        rows={ item.rows } 
+        sx={{
+          aspectRatio: `${ item.cols } / ${ item.rows }`,
+          border: 1, 
+          borderColor: 'grey.200', 
+          p: 2,
+          position: 'relative',
+        }}>
+        <Image
+          src={ item.img.src }
+          alt={ item.title }
+          loading='lazy'
+          fill
+          style={{ objectFit: 'cover' }}
+        />
+      </ImageListItem>
+    ))}
+  </ImageList>;
+}
 
 const ChirimenCrafts = () => {
-  const images = [ image154 ];
-  const imagesContainer = images.map((image, index) =>
-    <Grid size={ 4 } key={ index }>
-      <Image
-        src={ image.src }
-        width={ 935 }
-        height={ 616 }
-        alt='Chirimen-Craft Image'
-        style={{ objectFit: 'contain' }}
-      />
-    </Grid>
-  )
+  const chirimenCraftData = [
+    {
+      img: image154,
+      title: '',
+      rows: 1,
+      cols: 2,
+    },
+  ];
   return (
     <>
-      <Typography noWrap sx={{ 
-        fontFamily: '游明朝, Yu Mincho, Noto Serif JP', 
-        color: '#ff5a1e', 
-        fontSize: 'calc(0.4em + 1.0vw)',
-        textAlign: 'left'
-      }}>
-        ※シーズン商品は売り切れの場合がございます。ご了承ください。
-      </Typography>
-          <Paper>
-      <Grid container spacing={ 2 }>
-        { imagesContainer }
-      </Grid>
-    </Paper>
+      { makeImageListContent(chirimenCraftData, 2) }
     </>
   )
 }
