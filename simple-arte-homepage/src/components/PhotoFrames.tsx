5740dbfbf2aeefef808db57f5a662d0e974f3f5c
n-nob-at-GitHub <sub.nakamura.xyz@gmail.com>
2025-05-15 19:46:20 +0900

    簡易ホームページ追加対応（フォトフレームレイアウト修正）

--- simple-arte-homepage/src/components/PhotoFrames.tsx
+++ simple-arte-homepage/src/components/PhotoFrames.tsx
@@ -1,30 +1,91 @@
 'use client'
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 import {
-  Grid,
-  Paper,
+  ImageList,
+  ImageListItem,
 } from '@mui/material'
-import image117 from '@/images/117.png'
+import image116_1 from '@/images/116-1.webp'
+import image116_2 from '@/images/116-2.webp'
+import image117_1 from '@/images/117-1.webp'
+import image117_2 from '@/images/117-2.webp'
+import image117_3 from '@/images/117-3.webp'
+
+type ImageItem = {
+  img: StaticImageData
+  title: string
+  cols?: number
+  rows?: number
+}
+
+const makeImageListContent = (data: ImageItem[], maxCols = 1) => {
+  return <ImageList 
+    cols={ maxCols }
+    sx={{
+      '& .MuiImageListItem-root': {
+        position: 'relative',
+      },
+    }}>
+    { data.map((item, index) => (
+      <ImageListItem 
+        key={ index }
+        cols={ item.cols } 
+        rows={ item.rows } 
+        sx={{
+          aspectRatio: `${ item.cols } / ${ item.rows }`,
+          border: 1, 
+          borderColor: 'grey.200', 
+          p: 2,
+          position: 'relative',
+        }}>
+        <Image
+          src={ item.img.src }
+          alt={ item.title }
+          loading='lazy'
+          fill
+          style={{ objectFit: 'cover' }}
+        />
+      </ImageListItem>
+    ))}
+  </ImageList>;
+}
 
 const PhotoFrames = () => {
-  const images = [ image117 ];
-  const imagesContainer = images.map((image, index) =>
-    <Grid size={ 4 } key={ index }>
-      <Image
-        src={ image.src }
-        width={ 712 }
-        height={ 765 }
-        alt='Photo-Frame Image'
-        style={{ objectFit: 'contain' }}
-      />
-    </Grid>
-  )
+  const itemData = [
+    {
+      img: image116_1,
+      title: '',
+      rows: 2,
+      cols: 2,
+    },
+    {
+      img: image116_2,
+      title: '',
+      rows: 1,
+      cols: 2,
+    },
+    {
+      img: image117_1,
+      title: '',
+      rows: 1,
+      cols: 2,
+    },
+    {
+      img: image117_2,
+      title: '',
+      rows: 1,
+      cols: 2,
+    },
+    {
+      img: image117_3,
+      title: '',
+      rows: 1,
+      cols: 2,
+    }
+  ];
   return (
-    <Paper>
-      <Grid container spacing={ 2 }>
-        { imagesContainer }
-      </Grid>
-    </Paper>
+    <>
+      { makeImageListContent(itemData, 2) }
+    </>
   )
 }
 
